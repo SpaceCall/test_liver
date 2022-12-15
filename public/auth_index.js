ab070188d19a4e1b4fd642ece9f541c510e3866c
SpaceCall <93478572+SpaceCall@users.noreply.github.com>
2022-12-15 16:42:09 +0200

    Update auth_index.js

--- public/auth_index.js
+++ public/auth_index.js
@@ -1,5 +1,5 @@
 const socket_config = { transports: ['websocket']};
-const adress = '192.168.0.75';
+const adress = 'test-liver.vercel.app';
 const port = 1085;
 const socket = io( `http://${ adress }:${ port }` , socket_config );
 
