f330eeebd0ddca8e20d387a25f1f14a4055d17cd
SpaceCall <93478572+SpaceCall@users.noreply.github.com>
2023-01-27 03:14:46 +0200

    1

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
 function createServer(){
-    const PORT = 3000;
-    const express = require('express');
-    const path = require('path');
-    const app = express();
-    const createPath = (page) => path.resolve(__dirname,'public', `${page}.html`)
-    app.listen( PORT,(error)=>{
-        error ? console.log(error) : console.log( `server listening port ${PORT}` );
+    const fs = require( 'fs' );
+    const http = require( 'http' );
+    const server = http.createServer( onConnectionHttp );
+    const port = 8085;
+
+    const io = require('socket.io');
+    const ioServer = io( server );
+
+    ioServer.set('transports', ['websocket']);
+    ioServer.on('connection', onConnectSocket );
+
+
+    // обработка http запросов ( http сервер выдает файлы )
+    function onConnectionHttp( req, res ){
+
+        console.log( 'connection' );
+
+        if( req.url === '/' ){
+            fs.readFile('public/auth_index.html',(err,data)=>{
+                if( err ) console.log( err );
+                res.end( data );
+            });
+        }
+        else if( req.url === '/auth_style.css' ){
+            fs.readFile('public/auth_style.css',(err,data)=>{
+                if( err ) console.log( err );
+                res.end( data );
+            });
+        }
+        else if( req.url === '/auth_index.js' ){
+            fs.readFile('public/auth_index.js',(err,data)=>{
+                if( err ) console.log( err );
+                res.end( data );
+            });
+        }
+    }
+
+
+    // Обработка socket.io запросов
+    function onConnectSocket( socket ){
+        console.log('ioServer : new socket connected');
+        /*
+            схема обработки сообщений пользователя
+            socket - подключенный пользователь
+
+            socket.on('название канала связи',( полученные данные )=>{
+                // что-то делаем
+            });
+
+            // отправка юзеру данных
+            socket.emit( 'название канала связи', параметры );
+
+            // отправка данных всем юзерам, кроме текущего
+            socket.broadcast.emit( 'название канала связи', параметры );
+
+            // отправка данных всем юзерам
+            ioServer.emit( 'название канала связи', параметры );
+        */
+        /*---- тут установка прослушивания на действия ( обработка собщений сервером -------*/
+
+
+
+
+
+
+    }
+
+    server.listen( port,()=>{
+        console.log( 'server online' );
     });
-    app.use(express.static('public'));
-
-
-
-    app.get('/',(req, res)=>{
-        res.sendFile(createPath('auth_index'));
-    });
-
-
 }
-createServer();
+createServer()
--- public/auth_index.js
+++ public/auth_index.js
@@ -1,3 +1,20 @@
+'use strict';
+
+const socket = io();
+
+let auth_data = document.getElementById('form');
+socket.on('connect', ()=>{
+    console.log( 'socket connected' );
+});
+
+// подключаемся к серверу
+socket.connect();
+
+
+
+
+
+
 
 
 
